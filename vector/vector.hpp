74830ee1fbc36f332d0f02ac6cf39143b94b92cc
v.gromova <v.gromova@yadro.com>
2025-04-19 01:12:47 +0300

    Add base operation for vector

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
+template <typename T>
 class Vector
 {
 public:
     Vector() : capacity(0), size(0), data(nullptr) {}
-    Vector(const size_t size, const int value = 0);
-
+    Vector(const size_t size, const T value);
+    void push_back(const T& value);
+    void pop_back();
+    size_t get_size();
+    T& operator[](const size_t i);
     ~Vector();
+
 private:
-    void resize();
+    void resize(const size_t newSize);
 
     size_t capacity;
     size_t size;
-    int *data;
+    T *data;
 };
